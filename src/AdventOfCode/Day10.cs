93bbd02e2bbe9c28b792a34a1d0b1997f90f3040
Adam Rodger <adam.rodger@gmail.com>
2025-12-10 07:42:44 +0000

    day 10 p1

---
+++ src/AdventOfCode/Day10.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Utilities;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 10
+    /// </summary>
+    public class Day10
+    {
+        public int Part1(string[] input)
+        {
+            int total = 0;
+
+            foreach (string line in input)
+            {
+                string[] split = line.Split(' ');
+
+                int target = 0;
+
+                foreach ((int i, char c) in split[0][1..].Enumerate())
+                {
+                    if (c == '#')
+                    {
+                        target += (1 << i);
+                    }
+                }
+
+                List<int> buttons = [];
+
+                foreach (string section in split.Skip(1).TakeWhile(l => l.StartsWith('(')))
+                {
+                    int button = 0;
+
+                    foreach (int x in section.Numbers<int>())
+                    {
+                        button += 1 << x;
+                    }
+
+                    buttons.Add(button);
+                }
+
+                total += FewestButtonPresses(new Machine { Target = target, Buttons = buttons });
+            }
+
+            return total;
+        }
+
+        public int Part2(string[] input)
+        {
+            foreach (string line in input)
+            {
+                throw new NotImplementedException("Part 2 not implemented");
+            }
+
+            return 0;
+        }
+
+        private static int FewestButtonPresses(Machine machine)
+        {
+            Queue<(int State, int Presses)> queue = new();
+            HashSet<int> visited = new();
+
+            queue.Enqueue((0, 0));
+
+            while (queue.TryDequeue(out (int State, int Presses) current))
+            {
+                if (current.State == machine.Target)
+                {
+                    return current.Presses;
+                }
+
+                if (!visited.Add(current.State))
+                {
+                    // been here before in same or fewer presses
+                    continue;
+                }
+
+                // try every button
+                foreach (int button in machine.Buttons)
+                {
+                    queue.Enqueue((current.State ^ button, current.Presses + 1));
+                }
+            }
+
+            throw new InvalidOperationException("Well this is embarrassing... ran out of moves");
+        }
+
+        private class Machine
+        {
+            /// <summary>
+            /// Target state to switch on the machine
+            /// </summary>
+            public int Target { get; init; }
+
+            /// <summary>
+            /// Buttons, which are bit masks that can be applied to the current state to create a new state
+            /// </summary>
+            public ICollection<int> Buttons { get; init; }
+        }
+    }
+}
